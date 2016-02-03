94fbffde8582f4fa11ab43cb7e49f0c0046f1dd8
Ruud Schouwenaar <ruudschouwenaar@ruuds-macbook.local>
2016-02-03 12:19:32 +0100

    Initial Commit

---
+++ DistanceCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DistanceCalculator
+//
+//  Created by Ruud Schouwenaar on 03/02/16.
+//  Copyright © 2016 Ruud Schouwenaar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DistanceCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DistanceCalculator
+//
+//  Created by Ruud Schouwenaar on 03/02/16.
+//  Copyright © 2016 Ruud Schouwenaar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
